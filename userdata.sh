4286a64b20ede0a56199769ca07b65655bd9f545
Pan, Wen-Ming <372476388@qq.com>
2020-06-03 15:10:40 +0800

    remove invaild ip

--- userdata.sh
+++ userdata.sh
@@ -6,7 +6,7 @@ xz -d node-v12.16.0-linux-x64.tar.xz
 tar -xvf node-v12.16.0-linux-x64.tar
 ln -s /node-v12.16.0-linux-x64/bin/node /usr/bin/node
 ln -s /node-v12.16.0-linux-x64/bin/npm /usr/bin/npm
+echo "*/10 * * * * root /bin/bash /galatency/update.sh" >> /etc/crontab
 cd /galatency/node-ws/
 npm i
 node index.js
-echo "*/10 * * * * root /bin/bash /galatency/update.sh" >> /etc/crontab
